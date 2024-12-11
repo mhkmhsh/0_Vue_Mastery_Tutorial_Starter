4ea8e2e684d971051c77aabc94f5c6cd47712cbe
mhkmhsh <mmahesh12@gmail.com>
2024-12-11 13:11:12 -0500

    list rendering

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ const app = Vue.createApp({
         return {
               product: 'Socks',
             image: './assets/images/socks_green.jpg'
-            inventory: 0
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' },
         }
     }
 })
